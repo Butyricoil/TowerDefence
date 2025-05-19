7d6491842dcfe9fe069a906ebe58a6ecffbb6f85
ButyricOil <butyricoil@gmail.com>
2025-05-19 20:44:17 +0400

    fix installer

--- Assets/Scripts/Infrastructure/DI/GameInstaller.cs
+++ Assets/Scripts/Infrastructure/DI/GameInstaller.cs
@@ -4,14 +4,17 @@ public class GameInstaller : MonoInstaller
 {
     [SerializeField] private PlayerConfig _playerConfig;
     [SerializeField] private PlayerController _playerPrefab;
-    
+
     // ReSharper disable Unity.PerformanceAnalysis
     public override void InstallBindings()
     {
         // Configs
         Container.Bind<PlayerConfig>().FromInstance(_playerConfig).AsSingle();
-        
+
         // Camera
         Container.Bind<CameraController>().FromComponentInHierarchy().AsSingle();
+
+        // Player
+        Container.Bind<IPlayer>().To<PlayerController>().FromComponentInNewPrefab(_playerPrefab).AsSingle();
     }
-} 
+}
